a84cfdceaa0e8e8171568d016cd5f8bdee700ae6
henecia33 <hitzhangsiyu@163.com>
2017-05-04 11:47:30 +0800

    DONE!

--- bisRpts.R
+++ bisRpts.R
@@ -17,49 +17,6 @@ risk.data.all$DATA_DT <- as.Date(risk.data.all$DATA_DT)
 
 odbcClose(channel)
 
-# # data for test preparation
-# data.length = 50
-# lastDay <- seq(as.Date("2016-02-01"), length = 15, by = "1 month") - 1
-# DATA_DT <- as.character(lastDay[sample(1:length(lastDay), size = data.length, replace = T)])
-# BEGIN_DATE <- as.character(sample(seq(as.Date('2016/01/01'), as.Date('2017/03/31'), by = "day"), data.length))
-# 
-# RELOAN <- runif(data.length, 1, 10)
-# CNT <- floor(runif(data.length, 1, 1000))
-# BAL <- runif(data.length, 1, 10000)
-# OD_AMT <- runif(data.length, 1, 10000)
-# AP_AMT <- runif(data.length, 1, 10000)
-# SP_AMT <- runif(data.length, 1, 10000)
-# NEW_AMT <- runif(data.length, 1, 10000)
-# OD_PRINCIPAL <- runif(data.length, 1, 10000)
-# 
-# type.five <- c(0:4)
-# type.four <- c(0:3)
-# type.three <- c(0:2)
-# type.two <- c(0:1)
-# type.minus <- c(-1:3)
-# 
-# OVERDUE_STATUS_5 <- type.five[sample(1:length(type.five), size = data.length, replace = T)]
-# OVERDUE_STATUS_5_LAST <- type.five[sample(1:length(type.five), size = data.length, replace = T)]
-# 
-# OVERDUE_STATUS_3 <- type.three[sample(1:length(type.three), size = data.length, replace = T)]
-# OVERDUE_STATUS_3_LAST <- type.three[sample(1:length(type.three), size = data.length, replace = T)]
-# 
-# NEW_LOAN <- type.two[sample(1:length(type.two), size = data.length, replace = T)]
-# 
-# STATUS_LAST_MONTH <- type.three[sample(1:length(type.three), size = data.length, replace = T)]
-# STATUS_THIS_MONTH <- type.three[sample(1:length(type.three), size = data.length, replace = T)]
-# 
-# MATURITY_DAYS <- type.four[sample(1:length(type.minus), size = data.length, replace = T)]
-# 
-# OVERDUE_DAYS <- type.five[sample(1:length(type.five), size = data.length, replace = T)]
-# 
-# OVERDUE_FLAG <- type.two[sample(1:length(type.two), size = data.length, replace = T)]
-# 
-# data.test <- data.frame(DATA_DT, OVERDUE_STATUS_5, OVERDUE_STATUS_5_LAST, OVERDUE_STATUS_3, OVERDUE_STATUS_3_LAST,
-#                  NEW_LOAN, RELOAN, STATUS_LAST_MONTH, STATUS_THIS_MONTH, MATURITY_DAYS, OVERDUE_DAYS, OVERDUE_FLAG,
-#                  BEGIN_DATE, CNT, BAL, OD_AMT, AP_AMT, SP_AMT, NEW_AMT, OD_PRINCIPAL)
-
-
 
 # 资金变化 Output: table.1.cnt; table.1.amt
 # part.1 cnt
@@ -84,7 +41,7 @@ table.1.cnt <- Reduce(function(x,y) merge(x,y, by = "DATA_DT", all = T), list(ta
 table.1.cnt[is.na(table.1.cnt)] <- 0
 table.1.cnt$DIFF <- table.1.cnt$`CNT.1-1` - table.1.cnt$x
 table.1.cnt$DATA_DT <- paste(month(as.Date(table.1.cnt$DATA_DT) %m-% months(1)), month(table.1.cnt$DATA_DT), sep = "-") %>%
-  paste(c("汇总"), sep = "")
+  paste(c("月汇总"), sep = "")
 
 
 # part.2 amt
@@ -177,6 +134,12 @@ table.3.part.4 <- aggregate(DIFF_AP_AMT~DATA_DT, subset(data.table.3, OVERDUE_ST
 table.3 <- Reduce(function(x,y) merge(x,y, by = "DATA_DT", all = T), list(table.3.part.1, table.3.part.2,
                                                                           table.3.part.3, table.3.part.4))
 
+table.3$title <- paste(month(table.3$DATA_DT), "月回款", sep = "")
+table.3 <- table.3[c(ncol(table.3), 1:(ncol(table.3)-1))]
+
+table.3$DATA_DT <- paste(month(as.Date(table.3$DATA_DT) %m-% months(1)), month(table.3$DATA_DT), sep = "-") %>%
+  paste(c("月汇总"), sep = "")
+
 
 # 迁徙率月度情况 Output: table.4.1; table.4.2
 data.table.4 <- arrange(risk.data.all[c("DATA_DT", "OVERDUE_STATUS_5", "OVERDUE_STATUS_5_LAST" ,"CNT")], DATA_DT)
@@ -224,27 +187,28 @@ table.4.2$DATA_DT <- paste(month(as.Date(table.4.2$DATA_DT) %m-% months(1)), mon
   paste(c("月汇总"), sep = "")
 
 # 资产情况月度分析  Output: table.5.1; table.5.2
-data.table.5 <- arrange(risk.data.all[c("BEGIN_DATE", "OVERDUE_STATUS_3", "OD_AMT", "LOAN_M_CNT", "RELOAN", "NEW_AMT")], BEGIN_DATE)
+data.table.5 <- arrange(risk.data.all[c("DATA_DT", "BEGIN_DATE", "OVERDUE_STATUS_3", "OD_AMT", "LOAN_M_CNT", "RELOAN", "NEW_AMT")], BEGIN_DATE)
 data.table.5$LOAN_M_CNT <- substr(data.table.5$LOAN_M_CNT, 1, 7)
 
 table.5.1.1 <- arrange(aggregate(NEW_AMT~BEGIN_DATE, data.table.5, FUN = "sum"), BEGIN_DATE)
 
 
 table.5.1.2 <- arrange(aggregate(OD_AMT~BEGIN_DATE+LOAN_M_CNT,
-                                 subset(data.table.5, BEGIN_DATE >= as.Date("2016-01-01")), FUN = "sum"), BEGIN_DATE, LOAN_M_CNT)# %>%
-table.5.1.2<- setNames(reshape(table.5.1.2, idvar = "BEGIN_DATE", timevar = "LOAN_M_CNT", direction = "wide"),
-                       c("BEGIN_DATE", unique(table.5.1.2$LOAN_M_CNT)))
+                                 subset(data.table.5, BEGIN_DATE >= min(data.table.5$DATA_DT)), FUN = "sum"), BEGIN_DATE, LOAN_M_CNT) %>%
+  reshape(idvar = "BEGIN_DATE", timevar = "LOAN_M_CNT", direction = "wide")
 
-
-
-table.5.1 <- merge(table.5.1.1, table.5.1.2, all = T)
-table.5.1 <- table.5.1[which(table.5.1$NEW_AMT != 0),]
+table.5.1 <- merge(table.5.1.1, table.5.1.2, all.y = T)
 
 
 table.5.2 <- cbind(table.5.1[,1:2], table.5.1[,-1:-2]/table.5.1$NEW_AMT)
+table.5.2[,-1:-2] <- apply(table.5.2[,-1:-2], c(1,2), function(x) if(!(is.na(x))) percent(x, d = 2) else NA)
 
 table.5.1$BEGIN_DATE <- substr(table.5.1$BEGIN_DATE, 1, 7)
 table.5.2$BEGIN_DATE <- substr(table.5.2$BEGIN_DATE, 1, 7)
+name.table.5 <- as.numeric(substr(colnames(table.5.1)[-1:-3], 8, nchar(colnames(table.5.1)[-1:-3]))) + 1
+name.table.5 <- Reduce(function(x,y) paste(x,y, sep = ""), list("第", name.table.5, "个月"))
+colnames(table.5.1) <- c("新增放款月份", "新增放款金额", "当月", name.table.5)
+colnames(table.5.2) <- colnames(table.5.1)
 
 # 终止和到期情况 Output: table.6.all; table.6.ex
 # 到期？？？MATURITY_DAYS
@@ -285,6 +249,7 @@ table.7.part.2$type0 <- percent(table.7.part.2$OD_AMT.2/table.7.part.2$sum, d = 
 table.7.part.2$type1 <- percent(table.7.part.2$OD_AMT.3/table.7.part.2$sum, d = 2)
 table.7.part.2$type2 <- percent(table.7.part.2$OD_AMT.4/table.7.part.2$sum, d = 2)
 table.7.2 <- table.7.part.2[, -2:-5]
+table.7.2$DATA_DT <- substr(table.7.2$DATA_DT, 1, 7)
 
 data.table.7.3 <- arrange(risk.data.all[c("DATA_DT", "OVERDUE_STATUS_3", "OVERDUE_STATUS_5", "CNT", "SP_AMT", "OD_PRINCIPAL", "OD_AMT", "BAL")] %>%
   subset(OVERDUE_STATUS_3 != 2 & as.Date(DATA_DT) >= as.Date('2017-01-01')), DATA_DT)
@@ -317,22 +282,29 @@ table.7.part.3.7 <- arrange(aggregate(Ratio.AMT~DATA_DT+OVERDUE_STATUS_5, data.t
 
 table.7.3 <- Reduce(function(x,y) merge(x,y, all = T), list(table.7.part.3.1, table.7.part.3.2, table.7.part.3.3, table.7.part.3.4,
                                                                           table.7.part.3.5, table.7.part.3.6, table.7.part.3.7))
-summary.7.3 <- data.frame(t(sapply(split(table.7.3[,-1:-2], table.7.3$DATA_DT), function(x) lapply(x, sum))))
-# summary.7.3$CNT <- as.numeric(summary.7.3$CNT)
 
-diff.7.3 <- summary.7.3[-1,] - summary.7.3[-nrow(summary.7.3),]
-row.names(diff.7.3) <- row.names(summary.7.3)[-1]
-
-summary.7.3$DATA_DT <- row.names(summary.7.3)
-summary.7.3$OVERDUE_STATUS_5 <- NA
-summary.7.3 <- summary.7.3[,c(8:9,1:7)]
-table.7.3 <- arrange(rbind(table.7.3, summary.7.3), DATA_DT)
-
-
-
+summary.7.3 <- aggregate(table.7.3[,-1:-2], by = table.7.3["DATA_DT"], FUN = "sum")
 table.7.3$Ratio.CNT <- percent(table.7.3$Ratio.CNT, d = 2)
 table.7.3$Ratio.AMT <- percent(table.7.3$Ratio.AMT, d = 2)
-
+table.7.3$OVERDUE_STATUS_5 <- lapply(table.7.3$OVERDUE_STATUS_5, function(x) case_when(x == 0 ~ "正常",
+                                                                                       x == 2 ~ "一般",
+                                                                                       x == 3 ~ "催收",
+                                                                                       x == 4 ~ "严重"))
+diff.7.3 <- summary.7.3[-1,] - summary.7.3[-nrow(summary.7.3),]
+diff.7.3$DATA_DT <- summary.7.3$DATA_DT[-1]
+diff.7.3$Ratio.CNT <- NA
+diff.7.3$Ratio.AMT <- NA
+diff.7.3$OVERDUE_STATUS_5 <- c("增量")
+summary.7.3$OVERDUE_STATUS_5 <- c("汇总")
+summary.7.3$Ratio.CNT <- percent(summary.7.3$Ratio.CNT, d = 2)
+summary.7.3$Ratio.AMT <- percent(summary.7.3$Ratio.AMT, d = 2)
+sum.diff.7.3 <- rbind(summary.7.3, diff.7.3)
+sum.diff.7.3 <- sum.diff.7.3[,c(1,9,2:8)]
+table.7.3 <- arrange(rbind(table.7.3, sum.diff.7.3),DATA_DT)
+table.7.3$DATA_DT <- substr(table.7.3$DATA_DT, 1, 7)
+colnames(table.7.1) <- c("month", "正常", "一般", "催收", "严重")
+colnames(table.7.2) <- c("month", "一般", "催收", "严重")
+colnames(table.7.3) <- c("month", "状态", "未结清户数", "占比", "应还总额", "逾期本金", "逾期总额", "余额", "占比")
 
 # OUTPUT
 # template
@@ -343,55 +315,49 @@ file.copy(ExcelFile, template)
 # output to excel
 writeWorksheetToFile(template, data = table.1.cnt,
                      sheet = "资金变化", header = F,
-                     startRow = 2, startCol = 2)
+                     startRow = 2, startCol = 1)
 writeWorksheetToFile(template, data = c("金额"),
                      sheet = "资金变化", header = F,
-                     startRow = nrow(table.1.cnt) + 2, startCol = 2)
+                     startRow = nrow(table.1.cnt) + 2, startCol = 1)
 writeWorksheetToFile(template, data = table.1.amt,
                      sheet = "资金变化", header = F,
-                     startRow = nrow(table.1.cnt) + 3, startCol = 2)
+                     startRow = nrow(table.1.cnt) + 3, startCol = 1)
 writeWorksheetToFile(template, data = table.2,
                      sheet = "续贷逾期情况", header = F,
-                     startRow = 4, startCol = 2)
+                     startRow = 4, startCol = 1)
 writeWorksheetToFile(template, data = table.3,
                      sheet = "还款情况", header = F,
-                     startRow = 3, startCol = 3)
+                     startRow = 3, startCol = 1)
 writeWorksheetToFile(template, data = table.4.1,
                      sheet = "迁徙率月度情况", header = F,
-                     startRow = 2, startCol = 2)
+                     startRow = 2, startCol = 1)
 writeWorksheetToFile(template, data = table.4.2,
                      sheet = "迁徙率月度情况", header = F,
-                     startRow = 2, startCol = ncol(table.4.1)+2)
+                     startRow = 2, startCol = ncol(table.4.1) + 1)
 writeWorksheetToFile(template, data = table.5.1,
-                     sheet = "资产情况月度分析", header = F,
-                     startRow = 4, startCol = 2)
+                     sheet = "资产情况月度分析", header = T,
+                     startRow = 3, startCol = 1)
 writeWorksheetToFile(template, data = table.5.2,
-                     sheet = "资产情况月度分析", header = F,
-                     startRow = nrow(table.5.1)+4, startCol = 2)
+                     sheet = "资产情况月度分析", header = T,
+                     startRow = nrow(table.5.1)+4, startCol = 1)
 writeWorksheetToFile(template, data = table.6.all,
                      sheet = "终止和到期情况", header = F,
-                     startRow = 4, startCol = 2)
+                     startRow = 4, startCol = 1)
 writeWorksheetToFile(template, data = table.6.ex,
                      sheet = "终止和到期情况", header = F,
-                     startRow = 4, startCol = ncol(table.6.all)+2)
+                     startRow = 4, startCol = ncol(table.6.all) + 2)
 writeWorksheetToFile(template, data = table.7.1,
-                     sheet = "资产结构类数据", header = F,
-                     startRow = 3, startCol = 2)
+                     sheet = "资产结构类数据", header = T,
+                     startRow = 2, startCol = 1)
 writeWorksheetToFile(template, data = c("金额比"),
                      sheet = "资产结构类数据", header = F,
-                     startRow = nrow(table.7.1) + 3, startCol = 2)
-writeWorksheetToFile(template, data = c("month", "一般", "一般", "催收", "严重"),
-                     sheet = "资产结构类数据", header = F,
-                     startRow = nrow(table.7.1) + 4, startCol = 2)
+                     startRow = nrow(table.7.1) + 3, startCol = 1)
 writeWorksheetToFile(template, data = table.7.2,
-                     sheet = "资产结构类数据", header = F,
-                     startRow = nrow(table.7.1) + 5, startCol = 2)
-writeWorksheetToFile(template, data = c("状态", "未结清户数", "占比", "应还总额", "逾期本金", "逾期总额", "余额", "占比"),
-                     sheet = "资产结构类数据", header = F,
-                     startRow = nrow(table.7.1) + nrow(table.7.1) + 6, startCol = 2)
+                     sheet = "资产结构类数据", header = T,
+                     startRow = nrow(table.7.1) + 4, startCol = 1)
 writeWorksheetToFile(template, data = table.7.3,
-                     sheet = "资产结构类数据", header = F,
-                     startRow = nrow(table.7.1) + nrow(table.7.1) + 6, startCol = 2)
+                     sheet = "资产结构类数据", header = T,
+                     startRow = nrow(table.7.1) + nrow(table.7.1) + 6, startCol = 1)
 
 
 
